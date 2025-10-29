6d2b5b90156420dcf7389c9cf8aba1fc91034215
oscarmoreno219 <oscar.moreno@synergialabs.com.ec>
2025-10-29 01:19:57 -0500

    chore: setup project

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directories 
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directories and files created successfully"
+
+
+
